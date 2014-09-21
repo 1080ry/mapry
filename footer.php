6161405919c2866e7ac7daf85967fdab5fb71923
1080ry <yoshihisa_toyama@Yoshihisa-Toyama-no-MacBook-Air.local>
2014-09-22 00:23:33 +0900

    footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+<footer>
+ <div class="footer-wrap">
+  <div class="info">
+   <div class="box-address">
+    <div class="logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png" width="60" height="31" alt="1080ry.com"></div>
+    <p class="address">
+    <strong><?php bloginfo('name'); ?></strong><br>
+    <?php bloginfo('description'); ?></p>
+   </div>
+   <nav class="copyright">
+    <ul>
+     <li><a href="#">サイトマップ</a></li>
+     <li><a href="#">その他</a></li>
+     <li><a href="#">プライバシーポリシー</a></li>
+    </ul>
+    <p>Copyright (C) 2014 <?php bloginfo('name'); ?> All Rights Reserved.</p>
+   </nav>
+  </div>
+  <!-- /info -->
+ </div>
+</footer>
+</div>
+<!--/wrapper-->
+<div>
+<a href="#wrapper" class="btn_pt"><img src="<?php bloginfo('template_url'); ?>/img/btn_pt.png" height="52" width="95" alt="PAGETOP"></a>
+</div>
+<script src="<?php bloginfo('template_url'); ?>/js/analytics.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/1080.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/smoothScroll.js"></script>
