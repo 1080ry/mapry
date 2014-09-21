5b0f0d7fc9d0480a6e10d0c4cf847aae680ab068
1080ry <yoshihisa_toyama@Yoshihisa-Toyama-no-MacBook-Air.local>
2014-09-22 00:24:55 +0900

    js

---
+++ js/1080.js
@@ -0,0 +1,15 @@
+//pagetop
+$(document).ready(function() {
+    var pagetop = $('.btn_pt');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 700) {
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+    pagetop.click(function () {
+        $('body, html').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
